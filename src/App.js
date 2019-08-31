ef720019314db907b891ba20939c55fe2433cb18
Injo Baik <baikinjo.28@gmail.com>
2019-08-31 04:36:50 -0700

    collection, shop, signin added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css'
 
 import HomePage from './pages/homepage/homepage'
+import ShopPage from './pages/shop/shop'
+import Header from './components/header/header'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   )
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,7 +4,7 @@ import Directory from '../../components/directory/directory'
 import './homepage.scss'
 
 const HomePage = () => (
-  <div className="homepage">
+  <div className='homepage'>
     <Directory />
   </div>
 )
