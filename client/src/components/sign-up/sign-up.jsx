01277172bd3d9f7d655c7eb077d18c700cf05036
Injo Baik <baikinjo.28@gmail.com>
2019-09-05 13:48:12 -0700

    separating client and server

---
+++ client/src/components/sign-up/sign-up.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+
+import FormInput from '../form-input/form-input'
+import CustomButton from '../custom-button/custom-button'
+
+import { signUpStart } from '../../redux/user/user.actions'
+
+import './sign-up.scss'
+
+const SignUp = ({ signUpStart }) => {
+  const [userCredentials, setUserCredentials] = useState({
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const { displayName, email, password, confirmPassword } = userCredentials
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      alert("Passwords don't match")
+      return
+    }
+    signUpStart({ displayName, email, password })
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target
+
+    setUserCredentials({ ...userCredentials, [name]: value })
+  }
+
+  return (
+    <div className='sign-up'>
+      <h2 className='title'>I do not have a account</h2>
+      <span>Sign up with your email and password</span>
+      <form className='sign-up-form' onSubmit={handleSubmit}>
+        <FormInput
+          type='text'
+          name='displayName'
+          value={displayName}
+          onChange={handleChange}
+          label='Display Name'
+          required
+        />
+        <FormInput
+          type='email'
+          name='email'
+          value={email}
+          onChange={handleChange}
+          label='Email'
+          required
+        />
+        <FormInput
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+          label='Password'
+          required
+        />
+        <FormInput
+          type='password'
+          name='confirmPassword'
+          value={confirmPassword}
+          onChange={handleChange}
+          label='Confirm Password'
+          required
+        />
+        <CustomButton type='submit'>Sign Up</CustomButton>
+      </form>
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => ({
+  signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignUp)
--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import StripeCheckout from 'react-stripe-checkout'
 
 const StripeCheckoutButton = ({ price }) => {
@@ -6,8 +7,23 @@ const StripeCheckoutButton = ({ price }) => {
   const publishableKey = 'pk_test_wMweGbmRJj3FuHSKczosIAVk0020qQ3Lw9'
 
   const onToken = token => {
-    console.log(token)
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(res => {
+        alert('Payment Successful')
+      })
+      .catch(error => {
+        console.log('Pyament error: ', JSON.parse(error))
+        alert(
+          'There was an issue with your payment. Please make sure to use provided credit card information'
+        )
+      })
   }
 
   return (
